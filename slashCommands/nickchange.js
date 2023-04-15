2eedad890a3a7d7081a382be7594459f08f3442e
mikailerkart <99979466+mikailerkart@users.noreply.github.com>
2023-04-15 17:03:07 +0200

    nickchange komut update
    nickchange komut update

--- slashCommands/nickchange.js
+++ slashCommands/nickchange.js
@@ -18,14 +18,14 @@ module.exports = {
                 .setDescription('yaş gir')
                 .setRequired(true)),
 	async execute(client, interaction) {
+
+        let member = await interaction.options.getMember("user");
 		
         let role = await interaction.guild.roles.cache.find(r => r.name.toLowerCase() == "register staff")
 
         if (!interaction.member.roles.cache.has('944700546017804363')) return interaction.reply({ content: "Sen nick değiştiremezsin.", ephemeral: true});
 
         if (member.roles.cache.has(role.id)) return interaction.reply("Bu kişi'nin nickini değiştiremezsin.");
-
-        let member = await interaction.options.getMember("user");
 
         const isim = await interaction.options.getString("isim");
         const yaş = await interaction.options.getString("yaş");
